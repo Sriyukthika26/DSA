ab97a21f510a37fdc2e8ae85efcd8f994c8a3135
Sriyukthika26 <23je0967@iitism.ac.in>
2024-09-19 14:58:34 +0530

    ReverseLL

--- Linkedlist/Practice/Reverse_LL.cpp
+++ Linkedlist/Practice/Reverse_LL.cpp
@@ -87,8 +87,42 @@ void rev_ll(node*&head){
         prev = curr;
         curr = temp;
     }
+    curr->next = prev;
     head = curr;
 }
+
+// reverse linkedlist using recursion
+node * reversellRecursion(node* &head){
+    if(head==NULL || head->next == NULL) {
+        return head;
+    }
+    node * new_head = reversellRecursion(head->next);
+    head ->next->next = head;
+    head->next = NULL;
+    return new_head;
+}
+
+//reverse ll k nodes at a time
+node * revKnodes(node* &head, int k){
+
+    node* prev = NULL;
+    node* curr = head;
+    int ct = 0;
+    while(curr!= NULL && ct<k){
+        node * temp = curr->next;
+        curr->next = prev;
+        prev =  curr;
+        curr = temp;
+        ct++;
+    }
+    if(curr!=NULL){
+    node* new_head = revKnodes(curr, k);
+    head->next = new_head;
+    }
+
+    return prev;
+}
+
 
 int main(){
     LinkedList ll;
@@ -96,8 +130,11 @@ int main(){
     ll.inserAtLast(2);
     ll.inserAtLast(3);
     ll.inserAtLast(4);
+    ll.inserAtLast(5);
     disp(ll.head);
     rev_ll(ll.head);
+    disp(ll.head);
+    ll.head = revKnodes(ll.head,3);
     disp(ll.head);
 
 }
