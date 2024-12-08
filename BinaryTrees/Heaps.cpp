7d7b03253f4d9b9c2dccedebd489059ec811ab40
Sriyukthika26 <23je0967@iitism.ac.in>
2024-12-09 00:38:30 +0530

    new files

--- BinaryTrees/Heaps.cpp
+++ BinaryTrees/Heaps.cpp
@@ -1,128 +1,78 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class MaxHeap{
-    int heapsize;
-    int maxsize;
-public:
-    int *arr;
-    MaxHeap(int maxsize){
-        maxsize = maxsize;
-        heapsize=0;
-        arr = new int [maxsize];
-    }
+int parent(int i){
+    int p = (i-1)/2;
+    return p;
+}
 
-    int parent(int i){
-        return (i-1)/2;
+void upheapify(vector<int>&a, int i){
+    while(i!=0 && a[parent(i)]< a[i]){
+        swap(a[parent(i)],a[i]);
+        i = parent(i);
     }
-    int lchild(int i){
-        return 2*i+1;
-    }
-    int rchild(int i){
-        return 2*i+2;
-    }
-    int getsize(){
-        return heapsize;
-    }
-    int getmax(){
-        return arr[0];
-    }
+}
 
-    void upheapify(int i){                //down to up-- up heap
-        while(i!=0 && arr[parent(i)]<arr[i]){
-            swap(arr[parent(i)], arr[i]);
-            i = parent(i);
-        }
+void maxheapify(vector<int>&a, int i){
+    int l = 2*i+1;
+    int r = 2*i+2;
+    int largest = i;
+    if(l<a.size() && a[l]>a[i]) largest=l;
+    if(r<a.size() && a[r]>a[largest]) largest=r;
+    if(largest!=i){
+       swap(a[largest], a[i]);
+       maxheapify(a,largest);
+    } 
+}
+
+void insertheap(vector<int>&a, int key){
+    a.push_back(key);
+    int size = a.size();
+    upheapify(a,size-1);
+}
+
+void buildheap(vector<int>&a){
+    int length = a.size();
+    int strt = length/2+1;
+    for(int i = strt; i>=0;i--){
+        maxheapify(a,i);
     }
+}
 
-    void insert(int key){
-        if(heapsize==maxsize){
-            cout<<"Overflow\n";
-            return;
-        }
-        heapsize++;
-        int i=heapsize-1;
-        arr[i] = key;
-        upheapify(i);
+int extractmax(vector<int>&a){
+    int maxi = a[0];
+    swap(a[0],a[a.size()-1]);
+    a.pop_back();
+    maxheapify(a,0);
+    return maxi;
+}
+
+void maxheap_updatekey(vector<int>&a, int key, int i){
+    if(a[i]>key){
+        a[i]=key;
+        maxheapify(a,i);
     }
-
-    void print(){
-        for (int i = 0; i < heapsize; i++)
-        {
-            cout<<arr[i]<<" ";
-        }
-        cout<<"\n";
+    else{
+        a[i]=key;
+        upheapify(a,i);
     }
+}
 
-    void maxheapify(int i){    // up to down-downheap
-        int l = lchild(i);
-        int r = rchild(i);
-        int largest = i;
-        if(l<heapsize && arr[l]>arr[i]) largest = l;
-        if(r<heapsize && arr[r]>arr[largest]) largest = r;
-        if(largest!=i){
-            swap(arr[i], arr[largest]);
-            maxheapify(largest);
-        }
+void buildheap1(vector<int>a){
+    vector<int>b;
+    for(int i=0;i<a.size();i++){
+        insertheap(b,a[i]);
     }
-
-    void increasekey(int i, int new_value){
-        arr[i] = new_value;
-        upheapify(i);
+}
+//unstable sorting
+void heapsort(vector<int>&a){
+    buildheap(a);
+    for(int i= a.size()-1; i>0;i--){
+        swap(a[0], a[i]);
+        maxheapify(a,0);
     }
-
-
-    int extractmax(){
-        if(heapsize<=0) return INT_MIN;
-        if(heapsize==1){
-            heapsize--;
-            return arr[0];
-        }
-        int maxi = arr[0];
-        swap(arr[0], arr[heapsize-1]);
-        heapsize--;
-        maxheapify(0);
-
-        return maxi;
-    }
-
-    void deletekey(int i){
-        increasekey(i,INT_MAX);
-        extractmax();
-    }
-};
-    void heapssort(MaxHeap mh){
-
-        int n = mh.getsize();
-        while(mh.getsize()>1){
-            mh.extractmax();
-        }
-
-        for(int i=0;i<n;i++) cout<<mh.arr[i]<<" ";
-    }
+}
 
 int main(){
-    MaxHeap mh(15);
-    mh.insert(3);
-    mh.insert(2);
-    mh.insert(5);
-    mh.insert(7);
-    mh.insert(6);
-    mh.insert(1);
-
-    mh.print();
-    // cout<<mh.getmax()<<"\n";
-    // cout<<mh.getsize()<<"\n";
-
-    // cout<<mh.extractmax()<<"\n";
-    // mh.print();
-
-    // mh.increasekey(2,9);
-    // mh.print();
-
-    // mh.deletekey(2);
-    // mh.print();
-
-    heapssort(mh);
-
+    
 }
--- graphs/prims.cpp
+++ graphs/prims.cpp
@@ -1,60 +1,50 @@
-// program for Prim's Minimum Spanning Tree (MST) algorithm.
-// This is for adjacency matrix representation of the graph
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
 #define v 9
 
-// function to find the node with minimum weight from the set of nodes not yet included in MST
 int minkey(int key[], bool mst[]){
-    int min = INT_MAX, minidx= -1;
+    int mini = INT_MAX; int minidx = -1;
     for(int i=0;i<v;i++){
-        if(key[i]< min && mst[i]==false){
-            min = key[i], minidx = i;
+        if(key[i]<mini &&mst[i]==false){
+            mini = key[i]; minidx = i;
         }
     }
     return minidx;
 }
 
-// constructed MST stored in parent[]
 void printmst(int parent[], int key[]){
-    cout<<"EDGE \tPARENT \tWeight\n";
-	
-	for(int i=0; i<v; i++)
-	{
-		cout<<i<<"\t"<<parent[i]<< "\t" << key[i]<<endl;
-	}
+    cout<<"EDGE \tParent \tweight"<<"\n";
+    for(int i=0;i<v;i++){
+        cout<<i<<"\t"<<parent[i]<<"\t"<<key[i]<<"\n";
+    }
 }
 
 void MST(int* graph){
-    int parent[v]; // to store constructed MST
+    int parent[v];
     int key[v];
-    bool mst[v]; // check whether node is included in MST
-
+    bool mst[v];
+    
     for(int i=0;i<v;i++){
         key[i] = INT_MAX;
-        mst[i] = false;
-    } 
-    key[0] = 0;
-    parent[0] = -1;
-    
-    for(int i=0 ; i<v;i++){
-        int u = minkey(key, mst);  // pick the minmum weight node from set of nodes not yet included in mst
-        mst[u] = true;  //add this to mst
-        for(int j=0;j<v;j++){
-            int k = *((graph+u*v)+j);
-            if(k< key[j] && k!=0){
-                key[j] = k;
-                parent[j] = u;
-            }
-        } 
+        mst[i]=false;
     }
-
-    printmst(parent, key);
+    key[0]=0;
+    parent[0] = -1;
+    for(int i=0;i<v;i++){
+        int u =minkey(key,mst);
+        mst[u]=true;
+        for(int j=0;j<v;j++){
+            int k  = *((graph+u*v)+j);
+            if(k && k< key[j]){
+                key[j]=k;
+                parent[j]=u;
+            }
+        }
+        printmst(parent,key);
+    }
+    
 }
-
 int main(){
 	int grap[v][v] = {{0,4,0,0,0,0,0,8,0}, 
 					{4,0,8,0,0,0,0,11,0},
