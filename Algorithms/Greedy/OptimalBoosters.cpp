a5724390a7b407df2aa6b2947883ba7a0e5adce9
Sriyukthika26 <23je0967@iitism.ac.in>
2025-04-16 14:27:05 +0530

    optimalboosters

--- Algorithms/Greedy/OptimalBoosters.cpp
+++ Algorithms/Greedy/OptimalBoosters.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
+
+vector<vector<pair<int,int>>> tree;
 int boosterCount = 0;
+int lossLimit;
 
-int dfs(int node, int parent, vector<vector<pair<int,int>>>& tree, int L) {
-    int maxLoss = 0;
-
-    for (auto& edge : tree[node]) {
-        if (edge.first != parent) {
-            int childLoss = dfs(edge.first, node, tree, L) + edge.second;
-            if (childLoss > L) {
-                // Booster needed at child
-                boosterCount++;
-            } else {
-                maxLoss = max(maxLoss, childLoss);
-            }
-        }
+void dfs(int node, int parent, int accumulatedLoss) {
+    if (accumulatedLoss > lossLimit) {
+        // Place booster here
+        boosterCount++;
+        accumulatedLoss = 0; // Reset loss after booster
     }
 
-    return maxLoss;
+    for (auto edge : tree[node]) {
+        if (edge.first != parent) {
+            dfs(edge.first, node, accumulatedLoss + edge.second);
+        }
+    }
 }
 
 int main() {
     int n, L;
-    cout << "Enter number of nodes and loss threshold (L): ";
+    cout << "Enter number of nodes and loss tolerance level: ";
     cin >> n >> L;
+    lossLimit = L;
 
-    vector<vector<pair<int,int>>> tree(n);
+    tree.resize(n);
 
-    cout << "Enter edges (u v loss):" << endl;
-    for (int i = 0; i < n - 1; i++) {
+    cout << "Enter " << n - 1 << " edges (format: u v loss):\n";
+    for (int i = 0; i < n - 1; ++i) {
         int u, v, loss;
         cin >> u >> v >> loss;
         tree[u].push_back({v, loss});
         tree[v].push_back({u, loss});
     }
 
-    dfs(0, -1, tree, L); // assuming 0 is root
-    cout << "Minimum number of boosters required: " << boosterCount << endl;
+    // Start DFS from root node 0
+    dfs(0, -1, 0);
 
+    cout << "Minimum boosters needed: " << boosterCount << endl;
     return 0;
 }
